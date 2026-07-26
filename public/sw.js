59d50758aae0798161860cdde2569cca38b2c923
amnascubixmet-lgtm <amnas.cubixmet@gmail.com>
2026-07-26 09:32:06 +0530

    modified sw.js in public

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v1.0.0-1785038480009";
+const CACHE_VERSION = "v1.0.0-1785038511489";
 const STATIC_CACHE = `cardflow-static-${CACHE_VERSION}`;
 const PAGE_CACHE = `cardflow-pages-${CACHE_VERSION}`;
 
