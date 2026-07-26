6072af8feef6a610342924547d07dd217d0112c8
amnascubixmet-lgtm <amnas.cubixmet@gmail.com>
2026-07-26 09:19:22 +0530

    feat: configure Next.js framework and add service worker for offline support

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v1.0.0-1785004719142";
+const CACHE_VERSION = "v1.0.0-1785037706548";
 const STATIC_CACHE = `cardflow-static-${CACHE_VERSION}`;
 const PAGE_CACHE = `cardflow-pages-${CACHE_VERSION}`;
 
